507254ee8c9f5e7f3d7302229473d3fb0a079262
SumanthReddy1228 <sumanth0129@gmai.com>
2024-10-13 23:02:04 +0530

    window handling

---
+++ tests/ChildWindow.spec.js
@@ -0,0 +1,15 @@
+const {test} =require("@playwright/test");
+
+test("child window handling", async ({browser})=>{
+    const context= await browser.newContext();
+    const page=await context.newPage({viewport:null});
+    
+    await page.goto("https://demoqa.com/");
+    const [newPage]=await Promise.all([
+        context.waitForEvent('page'),
+        page.locator(".banner-image").click()
+    ]);
+    // await page.pause();
+    const newPageText=await newPage.locator("a[class='btn btn-primary-shadow btn-block']").textContent();
+    console.log(newPageText);
+})
